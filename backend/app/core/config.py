b7cbb3ac10da78f2bc24397be4ced1e97fd5e75c
vinh doan ngoc <doanngocvinh1@gmail.com>
2024-08-26 20:21:53 +0700

    fix: config base

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,24 +9,9 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_SECONDS: int = 60 * 60 * 24 * 7  # Token expired after 7 days
     SECURITY_ALGORITHM: str = "HS256"
 
-    POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "postgres"
-    POSTGRES_SERVER: str = "postgresql+psycopg2"
-    POSTGRES_DB: str = "postgres"
-    DATABASE_URI: str = ""
+    DATABASE_URI: str
     POSTGRES_CONNECTION_POOL_SIZE: int = 20
 
-    # @validator('DATABASE_URI', pre=True)
-    # def assemble_db_connection(cls, v, values):
-    #     if isinstance(v, str):
-    #         return v
-    #     return PostgresDsn.build(
-    #         scheme='postgresql',
-    #         username=values.get('POSTGRES_USER'),
-    #         password=values.get('POSTGRES_PASSWORD'),
-    #         host=values.get('POSTGRES_SERVER'),
-    #         path=f"{values.get('POSTGRES_DB') or ''}",
-    #     )
     class Config:
         env_file = ".env"
 
